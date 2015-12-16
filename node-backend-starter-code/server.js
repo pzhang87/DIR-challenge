2c1bc65514a4a91040fa5028418403dceaaa5d6a
Peter Zhang <pzhang87@gmail.com>
2015-12-15 19:33:03 -0500

    debug server.js

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -2,31 +2,33 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser'); //was missing
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public'))); //missing parens
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+}); //missing bracket + parens
 
 app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
+  if (!req.body.name || !req.body.oid){
     res.send("Error");
-    return
-  
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
-});
+    return;
+  } //missing bracket
 
-app.list(3000, function(){
+    var data = JSON.parse(fs.readFileSync('./data.json'));
+    data.push(req.body);
+    fs.writeFile('./data.json', JSON.stringify(data));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
+
+app.listen(3000, function(){ //corrected to app.listen()
   console.log("Listening on port 3000");
 });
