9ed7c3a5970a0f9f7af88a17ebc113635137012e
Peter Zhang <pzhang87@gmail.com>
2015-12-18 07:00:53 -0500

    complete code challenge

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -5,30 +5,34 @@ var path = require('path');
 var bodyParser = require('body-parser'); //was missing
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true })) //does this need to be true?
 app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, 'public'))); //missing parens
 
+// this is the route for favorites, but we aren't using it here
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 }); //missing bracket + parens
 
-app.get('favorites', function(req, res){
-  if (!req.body.name || !req.body.oid){
+app.post('/favorites', function(req, res){//shouldn't this be a post request?
+  if (!req.body.Title || !req.body.imdbID){
+    console.log(req.body)
     res.send("Error");
     return;
   } //missing bracket
 
+    // this pushes the retrieved data from the front-end into the json file serving as the back-end
     var data = JSON.parse(fs.readFileSync('./data.json'));
     data.push(req.body);
     fs.writeFile('./data.json', JSON.stringify(data));
     res.setHeader('Content-Type', 'application/json');
-    res.send(data);
+    // we don't actually want this to send data in the response, but maybe a redirect to the root works for now
+    res.redirect('/');
   });
 
-app.listen(3000, function(){ //corrected to app.listen()
-  console.log("Listening on port 3000");
+app.listen(4000, function(){ //corrected to app.listen(), changed to 4000 temporarily
+  console.log("Listening on port 4000");
 });
